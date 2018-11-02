0e6cd09f531a8c1abe7ffdd6257612c47ea18084
Alan Hunter <localpunter@sky.com>
2018-11-02 14:38:35 +0000

    first commit

---
+++ JavaProject_PetApp/PetApp/src/test/java/com/codeclan/example/PetApp/PetAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeclan.example.PetApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
