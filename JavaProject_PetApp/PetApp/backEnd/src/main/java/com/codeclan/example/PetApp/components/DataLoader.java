c5f85a03cfae628831cfbb57b5b0af30f0ae250d
Filip Kuszenin <filipkuszenin@gmail.com>
2018-11-03 18:34:20 +0000

    "DataLoader set up"

---
+++ JavaProject_PetApp/PetApp/backEnd/src/main/java/com/codeclan/example/PetApp/components/DataLoader.java
@@ -0,0 +1,17 @@
+package com.codeclan.example.PetApp.components;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    public DataLoader() {
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
